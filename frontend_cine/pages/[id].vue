e6ee42febbe249a255bdd6b058ef2c99e7efb124
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-03-13 08:19:53 +0100

    afegir animació de càrrega

--- frontend_cine/pages/[id].vue
+++ frontend_cine/pages/[id].vue
@@ -1,9 +1,9 @@
 <template>
-  
-  
+
+
     <div class="container">
-        
-        <div class="div-movie-cont" v-if="fetch_is_done">
+
+        <div class="div-movie-cont" v-if="fetch_is_done && fetchSeats_is_done">
             <h2 class="title">{{ movie_session.title }}</h2>
             <p class="year">{{ movie_session.year }}</p>
             <p class="rating">{{ movie_session.rating }}</p>
@@ -13,24 +13,26 @@
             <p class="genreId">{{ movie_session.genre_id }}</p>
         </div>
 
-        <div class="div-seats-cont">
+        <div class="div-seats-cont" v-if="fetch_is_done && fetchSeats_is_done">
             <div v-for="seat in  seats" :key="seat.id" class="div-seat-cont"
-            :class="{ 'div-seat-cont--clicked': isSelected(seat.id) }"
-            @click="seat_selected(seat.id)"
-            @double-click="seat_selected(seat.id)">
+                :class="{ 'div-seat-cont--clicked': isSelected(seat.id) }" @click="seat_selected(seat.id)"
+                @double-click="seat_selected(seat.id)">
 
-                <img src="../public/seat.svg" alt="" srcset=""
-                class="seat-icon"               
-                v-if="seat.status === 'available'">
+                <img src="../public/seat.svg" alt="" srcset="" class="seat-icon" v-if="seat.status === 'available'">
 
-                <img src="../public/seat_unavaliable.png" alt="" srcset=""
-                class="seat-icon"
-                v-if="seat.status === 'unavailable'">
+                <img src="../public/seat_unavaliable.png" alt="" srcset="" class="seat-icon"
+                    v-if="seat.status === 'unavailable'">
 
             </div>
-            <button class="btn-buy">COMPRAR</button>
+            <button class="btn-buy" @click="purchase_seats()" @double-click="purchase_seats()">
+                COMPRAR
+            </button>
         </div>
-     
+
+        <div v-if="!fetch_is_done || !fetchSeats_is_done" class="loading-container">
+            <img src="../public/loading.svg" alt="" srcset="" class="spinner-icon">
+        </div>
+
     </div>
 
 </template>
@@ -95,6 +97,10 @@ export default {
         isSelected(id) {
             return this.selected_seats.includes(id);
         },
+        purchase_seats() {
+            
+
+        }
     },
     created() {
         this.movie_session_id = this.$route.params.id;
@@ -107,6 +113,31 @@ export default {
 </script>
 
 <style scoped>
+.spinner-icon {
+    height: 100px;
+    border-top: 4px solid transparent;
+    animation: spin 1s linear infinite;
+    border: 4px solid #81b3c0d7;
+    border-radius: 50%;
+    border-top: 4px solid transparent;
+    /* Create a circular shape */
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+
 .container {
     display: grid;
     justify-content: center;
@@ -114,17 +145,20 @@ export default {
     grid-template-areas: ". div-movie-cont div-seats-cont .";
     height: auto;
 }
-.btn-buy{
+
+.btn-buy {
     border: 2px solid black;
-    border-radius: 10px;
+    border-radius: 12px;
     background-color: #dddddd62;
     font-size: 2.5em;
     cursor: pointer;
     width: 100%;
-    
+    ;
+
 }
-.btn-buy:hover{
-    
+
+.btn-buy:hover {
+    box-shadow: 0 0 4px 0 #000000d7;
 }
 
 .div-movie-cont {
@@ -174,8 +208,7 @@ export default {
 }
 
 .div-seat-cont:hover {
-   
-    box-shadow: 0px 1px 1px 1px #000000d7;
+    box-shadow: 0 0 4px 0 #000000d7;
     cursor: pointer;
 }
 
@@ -187,8 +220,8 @@ export default {
     margin: 10px;
 }
 
-.div-seat-cont--clicked{
-    background-color: rgba(218, 206, 206, 0.87);
+.div-seat-cont--clicked {
+    background-color: #81b3c0d7;
 
 }
 
@@ -218,7 +251,8 @@ export default {
     font-size: 1.5em;
     margin: 10px;
 }
-.poster{
+
+.poster {
     border-radius: 16px;
 }
 
